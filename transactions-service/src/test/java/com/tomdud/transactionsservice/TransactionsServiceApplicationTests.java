d962a35308ddaa44960f7ec5f4d395fafa1c5284
tomadud-developer <tomadud045@student.polsl.pl>
2023-02-09 20:47:37 +0100

    Initial commit, create transactions-service, add global gitignore

---
+++ transactions-service/src/test/java/com/tomdud/transactionsservice/TransactionsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomdud.transactionsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
