5eeaae03739eabbbf55b9e05ae82e2b89830da9a
tomadud-developer <tomadud045@student.polsl.pl>
2023-02-15 21:14:15 +0100

    Account model

---
+++ accounts-service/src/main/java/com/tomdud/accountsservice/model/Account.java
@@ -0,0 +1,28 @@
+package com.tomdud.accountsservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "account")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "account_name")
+    private String accountName;
+
+    @Column(name = "account_balance")
+    private Long balance;
+
+    @Column(name = "customer_id")
+    private Long customerId;
+}
